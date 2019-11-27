a955b66612780b06997efd3973fcaf1c3cb119a4
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-11-27 17:53:08 -0600

    fixed

---
+++ Java/src/problems/_121BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.java
@@ -0,0 +1,19 @@
+package problems._121BestTimetoBuyandSellStock;
+
+/**
+ * BestTimetoBuyandSellStock
+ */
+public class BestTimetoBuyandSellStock {
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        
+        int result = 0;
+
+        return result;
+    }
+}
--- Problems/0001_TwoSum/TwoSum.java
+++
@@ -1,7 +0,0 @@
-package 00001_TwoSum;
-
-public class TwoSum{
-    public static void main(String[] args) {
-        System.out.println("H")
-    }
-}
