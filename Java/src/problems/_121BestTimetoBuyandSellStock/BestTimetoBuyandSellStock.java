e997d1e6837e56f28178c17f3340e9a56ac54e35
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-11-27 18:30:56 -0600

    121

--- Java/src/problems/_121BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.java
+++ Java/src/problems/_121BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.java
@@ -6,14 +6,31 @@ package problems._121BestTimetoBuyandSellStock;
 public class BestTimetoBuyandSellStock {
 
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(maxProfit(prices));
+        int[] prices = {7,6,4,3,1};
+        //{7,1,5,3,6,4};
+        System.out.println(maxProfit1(prices));
     }
 
-    public static int maxProfit(int[] prices) {
-        
-        int result = 0;
 
-        return result;
+    //brute force
+    public static int maxProfit1(int[] prices) {
+        
+        int maxPro = 0;
+
+        for(int i = 0; i< prices.length-1; i++){
+
+            int profit = 0, buy = prices[i], sell;
+            for(int j = i+1; j< prices.length; j++){
+                sell = prices[j];
+                if(buy > sell) break;
+                int diff = sell - buy;
+                profit = Math.max(profit, diff);
+            }
+        
+            maxPro = Math.max(maxPro, profit);
+        }
+
+        return maxPro;
+        
     }
 }
