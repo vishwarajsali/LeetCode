2b43ea04d3e95782c00e10e409deeb87868e834a
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-11-28 01:29:29 -0600

    added test

---
+++ Java/src/test/java/problems/_1TwoSum/TwoSumTest.java
@@ -0,0 +1,30 @@
+package problems._1TwoSum;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+
+    @Test
+    void testTwoSum() {
+        TwoSum twoSum = new TwoSum();
+        int[][] input = {
+            {2,7,11,15},
+            {3,2,4}
+
+    };
+
+    int[] target = {9,6};
+    String[] excpected = {"[0, 1]","[1, 2]"};
+
+    for(int i = 0; i < input.length; i++){
+        String actual = Arrays.toString(twoSum.twoSum(input[i], target[i]));
+        String message = "input : " + Arrays.toString(input[i]) + "\n Target : "+ target[i] ;
+        assertEquals(excpected[i], actual, message);
+    }
+    }
+}
