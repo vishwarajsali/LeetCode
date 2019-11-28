7604bd3b55c64765255edc1d6f93fb98fc080fe9
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-11-28 11:06:36 -0600

    fix

---
+++ Java/src/main/java/problems/_155MinStack/MinStack.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+/**
+ * MinStack
+ */
+class MinStack {
+    /** initialize your data structure here. */
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<>();
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+}
---
+++ Java/src/main/java/problems/_1TwoSum/TwoSum.java
@@ -0,0 +1,4 @@
+package problems._1TwoSum;
+
+public class TwoSum {
+}
