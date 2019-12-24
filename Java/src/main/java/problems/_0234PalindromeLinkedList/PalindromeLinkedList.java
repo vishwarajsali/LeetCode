1e175cfe4ad250f7a73d714f2846b6acc1e768db
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-12-24 16:11:15 -0600

    234

---
+++ Java/src/main/java/problems/_0234PalindromeLinkedList/PalindromeLinkedList.java
@@ -0,0 +1,41 @@
+package problems._0234PalindromeLinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import LeetCodeLib.ListNode;
+
+/**
+ * PalindromeLinkedList
+ */
+public class PalindromeLinkedList {
+
+    public static void main(String[] args) {
+        int[] array = {-129,-129};
+
+        ListNode list =  ListNode.createArraytoList(array);
+
+        System.out.println(isPalindrome(list));
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        
+        List<Integer> list = new ArrayList<>();
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+
+        int end = list.size()-1,start = 0;
+
+        while(start < end){
+            if(!list.get(start++).equals(list.get(end--))) return false;
+            
+        }
+
+        return true;
+        
+
+    }
+}
