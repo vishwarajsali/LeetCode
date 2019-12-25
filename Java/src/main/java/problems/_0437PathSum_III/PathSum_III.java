7b4b3fd1dd3095e6a1d558c2238bf24a1ff066d9
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-12-25 11:26:44 -0600

    437

---
+++ Java/src/main/java/problems/_0437PathSum_III/PathSum_III.java
@@ -0,0 +1,40 @@
+package problems._0437PathSum_III;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import LeetCodeLib.TreeNode;
+
+/**
+ * PathSum_III
+ */
+public class PathSum_III {
+
+    public static void main(String[] args) {
+        int[] nums = {10,5,-3,3,2,11,3,-2,1};
+        TreeNode root = new TreeNode();
+
+        root.creatBTarray(nums);
+        System.out.println(pathSum(root, 8));
+        //root.preorder();
+        print(root);
+
+
+    }
+
+    public static int pathSum(TreeNode root, int sum) {
+       return 0;
+    }
+
+    public static void print(TreeNode root){
+        if (root != null) {
+            print(root.left);
+            System.out.println(root.val);
+            print(root.right);
+        }
+    }
+
+    
+
+
+}
