1a9cf2d0ca7921cfd8eabb84f8ac2c3c6c96c823
Vishwaraj Sali <vishwarajsali@VishwarajMBP.local>
2019-12-28 11:42:37 -0600

    581

---
+++ Java/src/main/java/problems/_0543DiameterofBinaryTree/DiameterofBinaryTree.java
@@ -0,0 +1,34 @@
+package problems._0543DiameterofBinaryTree;
+
+import LeetCodeLib.TreeNode;
+
+/**
+ * DiameterofBinaryTree
+ */
+public class DiameterofBinaryTree {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+
+        depth(root);
+        return path -1;
+    }
+    
+    int path = 1;
+    
+    private int depth(TreeNode root){
+        if(root == null) return 0;
+        
+        int left = depth(root.left);
+        int right = depth(root.right);
+        
+        path = Math.max(path, left+ right+1);
+        
+        return Math.max(left, right) +1;
+        
+    }
+}
