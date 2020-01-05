32c7e3c06fcc194195b8da744710f3cc09cdfdf3
Vishwaraj <sali.vishwaraj@gmail.com>
2020-01-05 10:06:29 -0600

    17

--- Java/src/main/java/problems/_0017LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.java
+++ Java/src/main/java/problems/_0017LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.java
@@ -1,9 +1,7 @@
 package problems._0017LetterCombinationsofaPhoneNumber;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * LetterCombinationsofaPhoneNumber
@@ -17,11 +15,9 @@ public class LetterCombinationsofaPhoneNumber {
 
     }
 
-    
+    // map with the values on the dial
     public static List<String> letterCombinations(String digits) {
-
         String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-
         List<String> result = new ArrayList<>();
         for (int i = 0; i < digits.length(); i++) {
             String s1 = map[Integer.parseInt(digits.substring(i, i + 1))];
@@ -39,6 +35,36 @@ public class LetterCombinationsofaPhoneNumber {
             String list1 = s1.substring(i, i + 1);
             for (int j = 0; j < s2.length; j++) {
                 list1 = s1.substring(i, i + 1);
+                list1 = s2[j] + list1;
+                list.add(list1);
+            }
+            if (list1.length() == 1)
+                list.add(list1);
+        }
+        return list;
+    }
+
+    // map with the ASCII values
+    public static List<String> letterCombinations1(String digits) {
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            int s1 = Integer.parseInt(digits.substring(i, i + 1));
+            String[] s2 = new String[result.size()];
+            result.toArray(s2);
+            result = combination(s1, s2);
+        }
+
+        return result;
+    }
+
+    private static List<String> combination(int s1, String[] s2) {
+        int[] map2 = { 0, 0, 97, 100, 103, 106, 109, 112, 116, 119, 123 };
+        List<String> list = new ArrayList<>();
+        for (int i = map2[s1]; i < map2[s1 + 1]; i++) {
+            String list1 = Character.toString((char) i);
+            for (int j = 0; j < s2.length; j++) {
+                list1 = Character.toString((char) i);
                 list1 = s2[j] + list1;
                 list.add(list1);
             }
