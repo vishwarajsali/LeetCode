c6a44b09617d6cd87dfdd4e4e458d248dc55c9dd
Vishwaraj <sali.vishwaraj@gmail.com>
2020-01-04 23:35:03 -0600

    17

--- Java/src/main/java/problems/_0017LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.java
+++ Java/src/main/java/problems/_0017LetterCombinationsofaPhoneNumber/LetterCombinationsofaPhoneNumber.java
@@ -17,26 +17,14 @@ public class LetterCombinationsofaPhoneNumber {
 
     }
 
-
+    
     public static List<String> letterCombinations(String digits) {
 
-        Map<String, String> map = new HashMap<String, String>() {
-            {
-                put("2", "abc");
-                put("3", "def");
-                put("4", "ghi");
-                put("5", "jkl");
-                put("6", "mno");
-                put("7", "pqrs");
-                put("8", "tuv");
-                put("9", "wxyz");
-    
-            }
-        };
-        
+        String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
         List<String> result = new ArrayList<>();
         for (int i = 0; i < digits.length(); i++) {
-            String s1 = map.get(digits.substring(i, i + 1));
+            String s1 = map[Integer.parseInt(digits.substring(i, i + 1))];
             String[] s2 = new String[result.size()];
             result.toArray(s2);
             result = combination(s1, s2);
@@ -45,8 +33,8 @@ public class LetterCombinationsofaPhoneNumber {
         return result;
     }
 
-    private static  List<String> combination(String s1, String[] s2) {
-         List<String> list = new ArrayList<>();
+    private static List<String> combination(String s1, String[] s2) {
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < s1.length(); i++) {
             String list1 = s1.substring(i, i + 1);
             for (int j = 0; j < s2.length; j++) {
