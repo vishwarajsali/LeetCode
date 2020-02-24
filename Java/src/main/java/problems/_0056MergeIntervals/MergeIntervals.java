ff9e8b0e507e06cd72806dc8f4e28dc21c941544
Vishwaraj <sali.vishwaraj@gmail.com>
2020-02-23 22:58:20 -0600

    56

--- Java/src/main/java/problems/_0056MergeIntervals/MergeIntervals.java
+++ Java/src/main/java/problems/_0056MergeIntervals/MergeIntervals.java
@@ -1,8 +1,7 @@
 package problems._0056MergeIntervals;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Arrays; 
 import java.util.List;
 
 /**
